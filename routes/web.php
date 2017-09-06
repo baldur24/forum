926c1da7834bb461962a18ed9135067b703544d8
baldur <baldur-24@hotmail.com>
2017-09-06 14:58:47 +0000

    heimaverk1

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,11 @@
 <?php
 
+Auth::routes();
+
+route::get('/threads' , function() {
+	return view('threads.index');
+});
+
+route::get('/threads/create' , function() {
+	return view('threads.create');
+});
