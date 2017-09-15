4a4bbc2bf24c89a15f398e3d7182cd98dc17b002
baldur <baldur-24@hotmail.com>
2017-09-15 10:04:35 +0000

    foreach threads

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 class ThreadsController extends Controller
 {
     public function index()
-    {
-    	return view('threads.index');
+    {	
+    	$threads = [
+    		['title' => 'Titill A', 'body' => 'Lorem ipsum dolor set.'],
+    		['title' => 'Titill B', 'body' => 'Þetta er B']
+    	];
+
+    	return view('threads.index', compact('threads'));
     }
     public function create()
     {
@@ -16,7 +21,6 @@ class ThreadsController extends Controller
     }
     public function show($id)
     {
-    	
     	return view('threads.show');
     }
 }
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -7,23 +7,21 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Forum Threads</div>
                     <div class="panel panel-default">
-                      <div class="panel-body">
-                        <h3>Jeep nær sjöfaldar söluna á 9 árum</h3>
-                            Það bílamerki Fiat Chrysler bílasamstæðunnar sem gengið hefur best á undanförnum árum er Jeep. Þar á bæ voru framleiddir 300.000 bílar árið 2009, en Fiat Chrysler býst við að framleiða rétt um 2 milljónir Jeep bíla á næsta ári. Það er næstum sjöföldun á framleiðslu Jeep bíla á aðeins 9 árum og vöxturinn frá þessu ári til næsta árs yrði 30%. 
-
-                            Forstjóri Fiat Chrysler, Sergio Marchionne, sér þó ekki fyrir sér að þessi miklu vöxtur muni hægja á sér og spáir því að sala Jeep bíla geti hæglega farið í 7 milljónir bíla á ári, enda sjái ekki fyrir endann á síaukinni eftirspurn eftir jepplingum og jeppum. Til samanburðar framleiddi Ford 6,7 milljónir bíla í fyrra.
+                        <div class="panel-body">
+                        @foreach ($threads as $thread)
+                            <h3>{{ $thread['title'] }}</h3>
+                        <div class='body'>
+                            {{ $thread['body'] }}
+                        @endforeach
+                        </div>
+                        </div>
                     </div>
-                    <div class="panel-body">
-                    </div>
-            </div>
-                      <div class="panel-body">
-                        <h3>Handtóku vígamenn sem undirbjuggu stórar árásir</h3>
-                            Lögregluyfirvöld á Spáni og í Marokkó hafa handtekið sex menn grunaðir um að skipuleggja umfangsmiklar hryðjuverkaárásir. Einn maður var handtekinn í Melilla og fimm í Marokkó en Melilla er lítið spænskt yfirráðasvæði í Marokkó.
-
-                    </div>
-                    <div class="panel-body">
-                    </div>
+                </div>                        
+            </div>         
         </div>
     </div>
 </div>
 @endsection
+
+
+                               
